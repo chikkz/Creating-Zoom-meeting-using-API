41dc87839d24392252bba1880aea3b12ba3ea23e
Sarthak Sharma <47621411+chikkz@users.noreply.github.com>
2020-07-17 15:24:17 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once "class-db.php";
+ 
+define('CLIENT_ID', 'B8l9b1IAROWk2ParVyblOw');
+define('CLIENT_SECRET', 'CAW8DaY7XFxSW0XL55Vx31nRvTKPr462');
+define('REDIRECT_URI', 'https://2b2b95597edc.ngrok.io/zoom/callback.php');
